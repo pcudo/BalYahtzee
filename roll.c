2b8d85cef13014e37a675e3a1adbfea4c3901f94
lsylvestre <35763057+lsylvestre@users.noreply.github.com>
2018-01-24 22:12:05 +0100

    Add files via upload

--- roll.c
+++ roll.c
@@ -3,23 +3,6 @@
 
 #include "roll.h"
 
-char * ptrCharAlloc(int nb);
-char * diceAlloc();
-
-/* rend un nombre aléatoire entre 0 et max */
-long randValue(int max);
-
-/* prend un tableau "patern" de longeur "nb" 
- et rend une copie de "patern" permutée aléatoirement */
-char * randomPermutation (char * patern,int nb);
-
-char * diceArrange ();
-char * listDuplicates(int nb_dice);
-char * getRoll(int nb_dice);
-void printRoll (char * s);
-void StatRoll (int precision);
-
-
 char * ptrCharAlloc(int nb){
 	char * ptr;
 	ptr = malloc (nb * sizeof(char));

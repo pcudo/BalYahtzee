e9d4ff11cb0f6d7fb478e464ebd1c6d8bd55ec4c
lsylvestre <35763057+lsylvestre@users.noreply.github.com>
2018-01-31 10:53:50 +0100

    Add files via upload

--- balyahtzee.h
+++ balyahtzee.h
@@ -21,40 +21,40 @@ typedef struct player player;
 typedef struct game game;
 
 struct game{
-  player * player1;
-  player * player2;
-  int turns;
+    player * player1;
+    player * player2;
+    int turns;
 };
 
 struct scoreSheet{
-  int ones;
-  int twos;
-  int threes;
-  int fours;
-  int fives;
-  int sixes;
-  int three_of_a_kind;
-  int four_of_a_kind;
-  int five_of_a_kind;
-  int six_of_a_kind;
-  int seven_of_a_kind;
-  int eight_of_a_kind;
-  int five_and_three;
-  int four_and_four;
-  int full;
-  int small_straight;
-  int big_straight;
-  int chance;
+    int ones;
+    int twos;
+    int threes;
+    int fours;
+    int fives;
+    int sixes;
+    int three_of_a_kind;
+    int four_of_a_kind;
+    int five_of_a_kind;
+    int six_of_a_kind;
+    int seven_of_a_kind;
+    int eight_of_a_kind;
+    int five_and_three;
+    int four_and_four;
+    int full;
+    int small_straight;
+    int big_straight;
+    int chance;
 };
 
 struct player{
-  char * name;
-  short int score;
-  scoreSheet * playerScore;
+    char * name;
+    short int score;
+    scoreSheet * playerScore;
 };
 
 void initGame();
-int * getRoll();
+int * Tour();
 scoreSheet * evalRoll(scoreSheet * previousScore, int * roll);
 void printRoll(char * roll);
 void nextTurn();

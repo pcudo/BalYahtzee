621dceaf159eb31f8ef2a1506c1cbf8e2b32dd4f
lsylvestre <35763057+lsylvestre@users.noreply.github.com>
2018-01-31 11:25:14 +0100

    Add files via upload

---
+++ balyahtzee.h
@@ -0,0 +1,65 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  balyahtzee.h
+ *
+ *    Description:  Header for BalYahtzee
+ *
+ *        Version:  1.0
+ *        Created:  01/24/2018 03:14:49 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Patrick Cudo (pcudo), patrick.cudo@etud.univ-paris8.fr
+ *        Company:  Feel Good Inc.
+ *
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+typedef struct scoreSheet scoreSheet;
+typedef struct player player;
+typedef struct game game;
+
+struct game{
+    player * player1;
+    player * player2;
+    int turns;
+};
+
+struct scoreSheet{
+    int ones;
+    int twos;
+    int threes;
+    int fours;
+    int fives;
+    int sixes;
+    int three_of_a_kind;
+    int four_of_a_kind;
+    int five_of_a_kind;
+    int six_of_a_kind;
+    int seven_of_a_kind;
+    int eight_of_a_kind;
+    int five_and_three;
+    int four_and_four;
+    int full;
+    int small_straight;
+    int big_straight;
+    int chance;
+};
+
+struct player{
+    char * name;
+    short int score;
+    scoreSheet * playerScore;
+};
+
+void initGame();
+int * tour();
+scoreSheet * evalRoll(scoreSheet * previousScore, int * roll);
+void printRoll(int * roll);
+void nextTurn();
+
